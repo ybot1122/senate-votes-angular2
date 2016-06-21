67d6004bbc2c86764ad9ee6717bb0cb7ddf57373
Toby Liu <ybot1122@gmail.com>
2016-06-20 22:04:11 -0700

    title and subtitle templating accomplished

--- app/app.component.js
+++ app/app.component.js
@@ -1,11 +1,12 @@
 (function(app) {
   app.TopView = ng.core.Component({
     selector: 'top-view',
-    template: '<h1>Senate Votes</h1>'
+    template: '<h1>{{title}}</h1><h2>{{subtitle}}</h2>'
   })
   .Class({
     constructor: function() {
-        
+      this.title = 'H.R.2578 Senate Vote Results';
+      this.subtitle = 'A bill making appropriations for the Departments of Commerce and Justice, Science, and Related Agencies for the fiscal year ending September 30, 2016, and for other purposes.';
     }
   });
 })(window.app || (window.app = {}));
