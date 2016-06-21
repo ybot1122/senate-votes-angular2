48228c20ea7532445bdc1beb477ea5afd02af77b
Toby Liu <ybot1122@gmail.com>
2016-06-20 21:42:36 -0700

    2 space tabs

--- app/app.component.js
+++ app/app.component.js
@@ -1,11 +1,11 @@
 (function(app) {
-    app.TopView = ng.core.Component({
-        selector: 'top-view',
-        template: '<h1>Senate Votes</h1>'
-    })
-    .Class({
-        constructor: function() {
-            
-        }
-    });
+  app.TopView = ng.core.Component({
+    selector: 'top-view',
+    template: '<h1>Senate Votes</h1>'
+  })
+  .Class({
+    constructor: function() {
+        
+    }
+  });
 })(window.app || (window.app = {}));
